3e7f81562f9f448dc4eaa80c0495eabecf9fe4e6
Guy J Anderson <88031354+GuyJiawei@users.noreply.github.com>
2023-04-03 18:59:45 +1000

    updated routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,19 +1,98 @@
 const router = require('express').Router();
-const {
-  createThought,
-  getThoughts,
-  getSingleThought,
-  addReaction,
-  deleteReaction,
-  deleteThought,
-} = require('../../controllers/thoughtController.js');
+const { Thoughts, Reactions, User } = require('../../models');
 
-router.route('/').get(getThoughts);
+// getting thoughts
+router.get('/', (req, res) => {
+    Thoughts.find()
+    .then(async (thoughts) => {
+      return res.json(thoughts);
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json(err);
+    });
+});
 
-router.route('/:thoughtId').get(getSingleThought).delete(deleteThought);
+// getting a single thought
+router.get('/:id', (req, res) => {
+    console.log(req.params.id)
+    Thoughts.findOne({ _id: req.params.id })
+        .select('-__v')
+        .then(async (thought) =>
+        !thought
+            ? res.status(404).json({ message: `No thought with ID ${req.params.id}` })
+            : res.json({ thought })
+        )
+        .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+        });
+});
 
-router.route('/create/:userId').post(createThought);
+// creating new thought
+router.post('/', (req, res) => {
+    Thoughts.create(req.body)
+        .then((thought) => res.json(thought))
+        .catch((err) => res.status(500).json(err));
+});
 
-router.route('/reactions/:thoughtId/').delete(deleteReaction).post(addReaction);
+// updating thought by id
+router.put('/:id', (req, res) => {
+    Thoughts.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: req.body },
+        { new: true }
+    ).then((thought) => 
+    !thought
+    ? res.status(404).json({ message: `No thought with ID ${req.params.id}` })
+    : res.json(thought)
+    )
+    .catch((err) => {
+        console.log(err);
+        return res.status(500)
+    });
+});
+
+// deleting a thought by id
+router.delete('/:id', (req, res) => {
+    Thoughts.findOneAndDelete({ _id: req.params.id })
+    .then((thought) =>
+      !thought
+        ? res.status(404).json({ message: 'No thought with that ID' })
+        : res.json({ message: 'Thought deleted!' })
+    )
+    .catch((err) => res.status(500).json(err));
+});
+
+// adding reactions to a thought
+router.put('/:id/reactions', (req, res) => {
+    Thoughts.findOneAndUpdate(
+        { _id: req.params.id },
+        { $addToSet: { reactions: req.body } },
+        { runValidators: true, new: true }
+      )
+        .then((thought) =>
+          !thought
+            ? res.status(404).json({ message: 'No user Thought with that ID :(' })
+            : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+});
+
+// delete a reaction from a thought
+router.delete('/:id/reactions/:reactionId', (req, res) => {
+    Thoughts.findOneAndUpdate(
+        { _id: req.params.id },
+        { $pull: { reactions: { reactionId: req.params.reactionId } } },
+        { runValidators: true, new: true }
+      )
+        .then((thought) =>
+          !thought
+            ? res.status(404).json({ message: 'No thought found with that ID :(' })
+            : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err)
+    );
+})
 
 module.exports = router;
